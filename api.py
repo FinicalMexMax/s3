3634341c2c020d804c29882cd9ff20a9b56f6e65
Alexander <cmetanin2006@gmail.com>
2024-10-04 00:08:40 +0500

    update debbug

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
+
+import uvicorn
 import boto3
 
 
@@ -23,21 +26,40 @@ s3_client = boto3.client(
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Или укажите разрешенные домены
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
-    s3_client.upload_fileobj(file.file, S3_BUCKET_NAME, file.filename)
-    return {"message": f"File '{file.filename}' uploaded successfully"}
+    try:
+        s3_client.upload_fileobj(file.file, S3_BUCKET_NAME, file.filename)
+        return {"message": f"File '{file.filename}' uploaded successfully"}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/download/{filename}")
 def download_file(filename: str):
-    s3_client.download_file(S3_BUCKET_NAME, filename, filename)
-    return FileResponse(path=filename)
+    try:
+        s3_client.download_file(S3_BUCKET_NAME, filename, filename)
+        return FileResponse(path=filename)
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.delete("/delete/{filename}")
 def delete_file(filename: str):
-    s3_client.delete_object(Bucket=S3_BUCKET_NAME, Key=filename)
-    return {"message": f"File '{filename}' deleted successfully"}
+    try:
+        s3_client.delete_object(Bucket=S3_BUCKET_NAME, Key=filename)
+        return {"message": f"File '{filename}' deleted successfully"}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
